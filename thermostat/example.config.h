f0b662abfc24ef163f36c3693e905e523e6f2db5
Varga Márk <mark@modcode.hu>
2020-02-13 23:30:37 +0100

    Add SQL log and switches

--- thermostat/example.config.h
+++ thermostat/example.config.h
@@ -1,2 +1,6 @@
 #define CONFIG_SSID "ssid"
 #define CONFIG_PASSWD "password"
+#define CONFIG_SQL_SERVER ""
+#define CONFIG_SQL_NAME ""
+#define CONFIG_SQL_PASSWD ""
+IPAddress server_addr(192,168,0,1);
--- thermostat/thermostat.ino
+++ thermostat/thermostat.ino
@@ -1,8 +1,8 @@
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 #include <DHT.h>
+#include <time.h>
 #include "config.h"
 
 #ifndef STASSID
@@ -12,134 +12,172 @@
 #define DHTTYPE DHT11
 #endif
 
+//Wifi
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
+//Time
+int timezone = 3;
+int dst = 0;
+
+//SQL
+WiFiClient client;                 // Use this for WiFi instead of EthernetClient
+MySQL_Connection conn(&client);
+MySQL_Cursor* cursor;
+char INSERT_SQL[] = "INSERT INTO `esp`.`log` (`date`, `temperature`, `humidity`) VALUES (FROM_UNIXTIME(%d), '%f', '%f');";
+char query[128];
+
+//DHP
 DHT dht(DHTPIN, DHTTYPE);
 
-ESP8266WebServer server(80);
-
+//Relays
 const int thermostat = 2;
 const int relay1 = 0;
 const int relay2 = 4;
 const int relay3 = 5;
 
-void handleRoot() {
-  server.send(200, "text/plain", "Thermostat 2000");
-  server.send(200, "text/plain", STASSID);
-}
+//Buttons
+const int button1 = 15;
+const int button2 = 13;
 
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+//Leds
+const int led1 = 12;
+const int led2 = 16;
+
+//Variables
+int ledState = LOW;
+int loadLed = HIGH;
+int buttonState;             
+int lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void getWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  /*IPAddress ip(192,168,0,111);   
+  IPAddress gateway(192,168,0,1);   
+  IPAddress subnet(255,255,255,0);   
+  WiFi.config(ip, gateway, subnet);*/
+  Serial.println("\nConnecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    loadLed = !loadLed;
+    digitalWrite(led1, loadLed);
+    digitalWrite(led2, !loadLed);
+    delay(500);
   }
-  server.send(404, "text/plain", message);
 }
 
-void setup(void) {
+void getSql() {
+  //IPAddress server_addr(xx,xx,xx,xx);       // IP of the MySQL *server* here
+  char user[] = CONFIG_SQL_USER;              // MySQL user login username
+  char password[] = CONFIG_SQL_PASSWD;
+
+  Serial.print("Connecting to SQL...  ");
+  if (conn.connect(server_addr, 3306, user, password)){
+    Serial.println("OK.");
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+  }else{
+    Serial.println("FAILED.");
+    while(true){
+      loadLed = !loadLed;
+      digitalWrite(led1, loadLed);
+      digitalWrite(led2, loadLed);
+      delay(1000);
+    }
+  }
+  cursor = new MySQL_Cursor(&conn);
+}
+
+void getTime(){
+  configTime(0 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+}
+
+void setRelay(){
   pinMode(thermostat, OUTPUT);
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
   digitalWrite(thermostat, 1);
   digitalWrite(relay1, 1);
   digitalWrite(relay2, 1);
   digitalWrite(relay3, 1);
+  
+}
+  
+void setup() {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  IPAddress ip(192,168,0,111);   
-  IPAddress gateway(192,168,0,1);   
-  IPAddress subnet(255,255,255,0);   
-  WiFi.config(ip, gateway, subnet);
-  WiFi.hostname("Thermostat");
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {;
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  //Serial.setDebugOutput(true);
+  getWifi();
+  getTime();
+  getSql();
+  setRelay();
 
   dht.begin();
-
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", handleRoot);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.on("/temp", []() {
-    float t = dht.readTemperature();
-    server.send(200, "text/plain", String(t));
-  });
-
-  server.on("/humi", []() {
-    float h = dht.readHumidity();
-    server.send(200, "text/plain", String(h));
-  });
-
-  server.on("/thermostat/on", []() {
-    digitalWrite(thermostat, 0);
-    server.send(200, "text/plain", "Relay ON");
-  });
-
-   server.on("/thermostat/off", []() {
-    digitalWrite(thermostat, 1);
-    server.send(200, "text/plain", "Relay OFF");
-  });
-
-  server.on("/relay1/on", []() {
-    digitalWrite(relay1, 0);
-    server.send(200, "text/plain", "Relay ON");
-  });
-  server.on("/relay1/off", []() {
-    digitalWrite(relay1, 1);
-    server.send(200, "text/plain", "Relay OFF");
-  });
-
-  server.on("/relay2/on", []() {
-    digitalWrite(relay2, 0);
-    server.send(200, "text/plain", "Relay ON");
-  });
-  server.on("/relay2/off", []() {
-    digitalWrite(relay2, 1);
-    server.send(200, "text/plain", "Relay OFF");
-  });
-
- server.on("/relay3/on", []() {
-    digitalWrite(relay3, 0);
-    server.send(200, "text/plain", "Relay ON");
-  });
-  server.on("/relay3/off", []() {
-    digitalWrite(relay3, 1);
-    server.send(200, "text/plain", "Relay OFF");
-  });
-
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
+  digitalWrite(led1, ledState);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
 }
 
-void loop(void) {
-  server.handleClient();
-  MDNS.update();
+void loop() {
+  time_t now = time(nullptr);
+  Serial.println(ctime(&now));
+  //Serial.println(now);
+
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println("°C");
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println("%");
+
+  if (conn.connected()){
+    sprintf(query, INSERT_SQL, now, t, h);
+    //cursor->execute(query);
+  }
+
+  int reading = digitalRead(button1);
+  
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  digitalWrite(led1, ledState);
+  lastButtonState = reading;
+
+  if (digitalRead(button2) == HIGH) {
+    // turn LED on:
+    digitalWrite(led2, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(led2, LOW);
+  }
+  
+  //delay(1000);
+  int waitMin = 5;
+  //delay(waitMin*60*1000);
 }
